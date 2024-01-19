59d644121e9440db61e51a436ac44339404d317c
HarshitDave <daveharshit1@gmail.com>
2024-01-19 09:30:18 +0000

    resolved hydration issue

--- src/app/(chrono)/timeline.js
+++ src/app/(chrono)/timeline.js
@@ -1,22 +1,33 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Chrono } from "react-chrono";
 
 const TimeLine = ({ children, ...props }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <Chrono
-      {...props}
-      mode="VERTICAL"
-      hideControls
-      theme={{
-        primary: "black",
-        secondary: "none",
-        cardBgColor: "none",
-        cardForeColor: "black",
-        titleColor: "black",
-        titleColorActive: "black",
-      }}>
-      {children}
-    </Chrono>
+    <>
+      {isClient && (
+        <Chrono
+          {...props}
+          mode="VERTICAL"
+          hideControls
+          theme={{
+            primary: "black",
+            secondary: "none",
+            cardBgColor: "none",
+            cardForeColor: "black",
+            titleColor: "black",
+            titleColorActive: "black",
+          }}>
+          {children}
+        </Chrono>
+      )}
+    </>
   );
 };
 
